8981707f92cdb1b04fce6e1b718a3bc85bd8dc3a
Samuel Luiz <30871470+samuelluiz@users.noreply.github.com>
2019-07-11 16:58:15 -0300

    Add files via upload

---
+++ testeEOD.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int i;
+char texto[20], c, texto2[20] = "Hello World!";
+FILE *pont_arq;
+
+    pont_arq = fopen("teste.txt", "rb");
+
+// Testa abertura do arquivo
+    if(pont_arq == NULL){
+         printf("ERRO na abertura do arquivo.\n");
+        } else {
+            printf("Arquivo criado com sucesso \n");
+        }
+
+// Lê bytes do arquivo
+    while (c != EOF){
+        c = getc(pont_arq);
+        printf("%c", c);
+        texto[i] = c;
+        i++;
+    }
+// Escreve no arquivo
+    fprintf(pont_arq, "%s", texto2);
+
+fclose(pont_arq);
+
+return 0;
+}
